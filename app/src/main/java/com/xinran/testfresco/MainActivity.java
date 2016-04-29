971bbfc28cd346969ad8ed84b2c79349f54c824b
qixinh <qixinh@jumei.com>
2016-04-29 18:39:04 +0800

    add coed

--- app/src/main/java/com/xinran/testfresco/MainActivity.java
+++ app/src/main/java/com/xinran/testfresco/MainActivity.java
@@ -1,16 +1,50 @@
 package com.xinran.testfresco;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.AbstractDraweeController;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.drawee.controller.ControllerListener;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.image_view)
+    SimpleDraweeView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        downLoadImgWithHierarchy("http://pic1.nipic.com/2008-09-08/200898163242920_2.jpg");
+
+    }
+
+    /**
+     * 当然上面说到它强大了，强大就不止只是加载网络图片吧，它本身也支持本地，Content Provider，asset，res的图片
+     * <p/>
+     * 本地：file://
+     * Contentprovider:  content://
+     * asset:  asset://
+     * resouse: res://,
+     * 其实就是让你拼一个uri
+     */
+    private void downLoadImgSimple(String url) {
+        Uri uri = Uri.parse(url);
+        imageView.setImageURI(uri);
     }
 
     @Override
@@ -20,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * 显示图片的时候直接写了一个setImageURI（uri），
+     * Fresco不仅仅提供了这一个方法来显示图片，它还提供了setController（controller）方法加载图片
+     * 当然如果你想监听加载的过程，就加一个ControllerListener
+     *
+     *
+     *
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -34,4 +76,71 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private ControllerListener mControllerListener = new BaseControllerListener() {
+        @Override
+        public void onFailure(String id, Throwable throwable) {
+            super.onFailure(id, throwable);
+            Toast.makeText(MainActivity.this, "加载图片失败" + throwable.getMessage(), Toast.LENGTH_LONG);
+        }
+
+        @Override
+        public void onFinalImageSet(String id, Object imageInfo, Animatable animatable) {
+            super.onFinalImageSet(id, imageInfo, animatable);
+            Toast.makeText(MainActivity.this, "加载图片成功", Toast.LENGTH_LONG);
+        }
+
+        //如果图片设置渐进式
+        @Override
+        public void onIntermediateImageSet(String id, Object imageInfo) {
+            super.onIntermediateImageSet(id, imageInfo);
+        }
+
+        @Override
+        public void onIntermediateImageFailed(String id, Throwable throwable) {
+            super.onIntermediateImageFailed(id, throwable);
+        }
+
+        @Override
+        public void onRelease(String id) {
+            super.onRelease(id);
+        }
+
+        @Override
+        public void onSubmit(String id, Object callerContext) {
+            super.onSubmit(id, callerContext);
+        }
+    };
+
+    private void downLoadImgWithDraweeController(String url) {
+        AbstractDraweeController draweeController = Fresco.newDraweeControllerBuilder()
+                .setUri(url)
+                .setControllerListener(mControllerListener)
+                .build();
+        imageView.setController(draweeController);
+    }
+
+    /**
+     * SimpleDraweeView imageView许多xml可以设置的属性通过GenericDraweeHierarchy都可以设置
+     * 而且比xml更多
+     *
+     *
+     *  例如，我可以设置多张背景图片，我可以设置多张叠加图，这里都可以帮你实现，是不是很强大啊，
+     *  想不想拿到特权了一样呢！但是DraweeHiererchy创建时比较耗时，所以要多次利用
+
+        GenericDraweeHierarchy hierarchy1 = imageView.getHierarchy();
+        这个框架不仅仅是这些东西，它还有很多更牛逼的东西，例如：它提供了渐进式加载图片，显示gif动画图片等等
+     */
+    private void downLoadImgWithHierarchy(String url) {
+        GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(getResources())
+                .setFadeDuration(300)
+                .setBackground(getResources().getDrawable(R.mipmap.guide1))
+                .setOverlay(getDrawable(R.mipmap.ic_launcher))
+                .setPlaceholderImage(getDrawable(R.mipmap.guide3))
+                .setFailureImage(getDrawable(R.mipmap.ic_launcher))
+                .build();
+        imageView.setHierarchy(hierarchy);
+        downLoadImgWithDraweeController(url);
+    }
+
 }
--- app/src/main/java/com/xinran/testfresco/QxApp.java
+++ app/src/main/java/com/xinran/testfresco/QxApp.java
@@ -3,6 +3,9 @@ package com.xinran.testfresco;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.decoder.ProgressiveJpegConfig;
+import com.facebook.imagepipeline.image.QualityInfo;
 
 /**
  * Created by qixinh on 16/4/29.
@@ -11,6 +14,28 @@ public class QxApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fresco.initialize(getApplicationContext());
+        /**
+         * 首先是渐进式图片加载，这方面的功能充分考虑了网络比较慢的情况下，用户不至于一致在等，
+         * 最起码能看到模糊的照片，这个所谓的渐进式加载就是说用户从图片加载之后，图片会从模糊到清晰的一个渐变过程，
+         * 当然这个过程仅限于从网络加载图片，本地或者缓存等地方的图片也不需要渐进式加载，没有意义
+         *
+         * 当然你也可以使用ProgressiveJpegConfig config1= new SimpleProgressiveJpegConfig(list,2);
+         */
+        ProgressiveJpegConfig config = new ProgressiveJpegConfig() {
+            @Override
+            public int getNextScanNumberToDecode(int i) {
+                return 0;
+            }
+
+            @Override
+            public QualityInfo getQualityInfo(int i) {
+                return null;
+            }
+        };
+
+        ImagePipelineConfig imagePipelineConfig = ImagePipelineConfig.newBuilder(this)
+                .setProgressiveJpegConfig(config)
+                .build();
+        Fresco.initialize(getApplicationContext(),imagePipelineConfig);
     }
 }
