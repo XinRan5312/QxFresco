5440c595a338b7587b398a6fe1869db28f0a5a33
qixinh <qixinh@jumei.com>
2016-04-30 11:00:27 +0800

    WR add code

--- app/src/main/java/com/xinran/testfresco/MainActivity.java
+++ app/src/main/java/com/xinran/testfresco/MainActivity.java
@@ -9,18 +9,22 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
 import com.facebook.drawee.controller.AbstractDraweeController;
 import com.facebook.drawee.controller.BaseControllerListener;
 import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.generic.GenericDraweeHierarchy;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+String urlBig="http://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&pn=14&spn=0&di=150994728280&pi=&rn=1&tn=baiduimagedetail&is=&istype=0&ie=utf-8&oe=utf-8&in=&cl=2&lm=-1&st=undefined&cs=611483611%2C2895064642&os=2193683596%2C3063830214&simid=0%2C0&adpicid=0&ln=1000&fr=&fmq=1461982725006_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&ist=&jit=&cg=&bdtype=0&oriquery=&objurl=http%3A%2F%2Fimg05.tooopen.com%2Fimages%2F20150531%2Ftooopen_sy_127457023651.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3Fooo_z%26e3Bp555rjg_z%26e3Bv54AzdH3FetjoAzdH3Fld8nd0_z%26e3Bip4s&gsm=0&rpstart=0&rpnum=0";
+    String urlSmall="http://pic1.nipic.com/2008-09-08/200898163242920_2.jpg";
     @Bind(R.id.image_view)
     SimpleDraweeView imageView;
 
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        downLoadImgWithHierarchy("http://pic1.nipic.com/2008-09-08/200898163242920_2.jpg");
+        downLoadImgWithPipeline(urlSmall);
 
     }
 
@@ -87,13 +91,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onFinalImageSet(String id, Object imageInfo, Animatable animatable) {
             super.onFinalImageSet(id, imageInfo, animatable);
-            Toast.makeText(MainActivity.this, "加载图片成功", Toast.LENGTH_LONG);
+            Toast.makeText(MainActivity.this, "加载图片成功", Toast.LENGTH_SHORT);
         }
 
         //如果图片设置渐进式
         @Override
         public void onIntermediateImageSet(String id, Object imageInfo) {
             super.onIntermediateImageSet(id, imageInfo);
+            Toast.makeText(MainActivity.this, "渐进式", Toast.LENGTH_SHORT);
         }
 
         @Override
@@ -143,4 +148,38 @@ public class MainActivity extends AppCompatActivity {
         downLoadImgWithDraweeController(url);
     }
 
+    /**
+     * PipelineDraweeController是AbstractDraweeController的子类
+     *
+     * 当我们要从服务器端下载一张高清图片，图片比较大，下载很慢的情况下有些服务器会提供一张缩略图，
+     * 同样的Fresco也支持这种方法，在controller中提供了两个不同的方法setLowResImageRequest和setImageRequest，
+     * 看到方法名你应该明白了怎么用
+     * @param url
+     */
+private void downLoadImgWithPipeline(String url){
+    ImageRequest requestLow = ImageRequestBuilder
+            .newBuilderWithResourceId(R.mipmap.ic_launcher)
+            .setProgressiveRenderingEnabled(true)
+            .build();
+    ImageRequest request = ImageRequestBuilder
+            .newBuilderWithSource(Uri.parse(url))
+            .setProgressiveRenderingEnabled(true)
+            .build();
+    GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(getResources())
+            .setFadeDuration(300)
+            .setBackground(getResources().getDrawable(R.mipmap.guide1))
+            .setOverlay(getDrawable(R.mipmap.ic_launcher))
+            .setPlaceholderImage(getDrawable(R.mipmap.guide3))
+            .setFailureImage(getDrawable(R.mipmap.ic_launcher))
+            .build();
+    imageView.setHierarchy(hierarchy);
+    PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+            .setAutoPlayAnimations(true)
+            .setLowResImageRequest(requestLow)//缩略图
+           .setImageRequest(request)//高清图
+            .setOldController(imageView.getController())
+            .build();
+
+    imageView.setController(controller);
+}
 }
--- app/src/main/java/com/xinran/testfresco/QxApp.java
+++ app/src/main/java/com/xinran/testfresco/QxApp.java
@@ -8,6 +8,10 @@ import com.facebook.imagepipeline.decoder.ProgressiveJpegConfig;
 import com.facebook.imagepipeline.image.QualityInfo;
 
 /**
+ * 个人认为这个框架最巧妙的地方，就是把bitmap保存到ashmen，不会启动gc，
+ * 使的界面不会因为gc而卡死，Fresco使用三级缓存，第一级缓存就是保存bitmap，
+ * 第二级缓存保存在内存，但是没有解码，使用时需要界面，第三级缓存就是保存在本地文件，
+ * 同样文件也未解码，使用的时候要先解码啦！
  * Created by qixinh on 16/4/29.
  */
 public class QxApp extends Application {
@@ -20,6 +24,18 @@ public class QxApp extends Application {
          * 当然这个过程仅限于从网络加载图片，本地或者缓存等地方的图片也不需要渐进式加载，没有意义
          *
          * 当然你也可以使用ProgressiveJpegConfig config1= new SimpleProgressiveJpegConfig(list,2);
+         *
+         * 这个image pipeline这个又是个什么啊？它的来头比较大，负责图片的加载工作
+
+         1.检查内存缓存，如有，返回
+
+         2.后台线程开始后续工作
+
+         3.检查是否在未解码内存缓存中。如有，解码，变换，返回，然后缓存到内存缓存中。
+
+         4.检查是否在文件缓存中，如果有，变换，返回。缓存到未解码缓存和内存缓存中。
+
+         5.从网络或者本地加载。加载完成后，解码，变换，返回。存到各个缓存中。
          */
         ProgressiveJpegConfig config = new ProgressiveJpegConfig() {
             @Override
